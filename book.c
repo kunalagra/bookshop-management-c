e635bb31d33d39afb352f1292ce02f2f6670a95e
Kunal Agrawal <kevin.john77-01@yahoo.com>
2021-06-22 00:38:30 +0530

    Added BookID Search, Minor Refactor & changes

--- book.c
+++ book.c
@@ -7,69 +7,72 @@ struct bookinfo{
 	char name[30],author[20];
 	int id, price;
 }book[1000];
+
+int notfound(){
+	printf("\nBook ID not found!");
+	printf("\nReturning to Main Menu..");
+	sleep(4);
+	return 0;
+}
 	
 int choiceCase(int choice){
 	FILE *fcs;
+	system("cls");
+	fcs = fopen("data.csv","r");
+	char line[1024];
+	int j=0,inid,foundflag=0;
+	char *bid,*bname,*bauth,*bprice;
+	printf("\nReading Books Record..\n");
+	while (fgets(line, 1024, fcs)!=NULL){
+		bid = strtok(line,",");
+		bname=strtok(NULL, ",");
+		bauth=strtok(NULL, ",");
+		bprice=strtok(NULL, ",");
+		book[j].id = atoi(bid);
+		strcpy(book[j].name,bname);
+		strcpy(book[j].author,bauth);
+		book[j].price = atoi(bprice);
+		j++;
+	}
+	fclose(fcs);
 	switch (choice){
 		case 1: 
-			system("cls");
-			int bnum,i;
 			fcs = fopen("data.csv","a+");
 			printf("\nNumber Of Books to Add: ");
-			scanf("%d",&bnum);
-			for(i=0;i<bnum;i++){
-				printf("Enter Book Id: ");
+			scanf("%d",&inid);
+			int i;
+			for(i=0;i<inid;i++){
+				printf("\nEnter Book Id: ");
 				scanf("%d",&book[i].id);
 				getchar();
-				printf("\nEnter Book name: ");
+				printf("Enter Book name: ");
 				gets(book[i].name);
-				printf("\nEnter Book Author: ");
+				printf("Enter Book Author: ");
 				gets(book[i].author);
-				printf("\nEnter Book Price (in rupee): ");
+				printf("Enter Book Price (in rupee): ");
 				scanf("%d",&book[i].price);
 				fprintf(fcs, "%d,%s,%s,%d\n", book[i].id,book[i].name,book[i].author,book[i].price);
 			}
 			fclose(fcs);
-			printf("Started Writing.");
-			printf("\nCompleted Writing");
-			printf("\nGoing back to main menu..");
+			printf("\nNew Records have been added successfully");
+			printf("\nReturning to main menu..");
 			sleep(3);
 			return 0;
 		case 2:
-			system("cls");
-			fcs = fopen("data.csv","r");
-			char line[1024];
-			int j=0,recnum;
-			char *bid,*bname,*bauth,*bprice;
-			printf("\nReading Books Record..");
-			while (fgets(line, 1024, fcs)!=NULL){
-				bid = strtok(line,",");
-				bname=strtok(NULL, ",");
-				bauth=strtok(NULL, ",");
-				bprice=strtok(NULL, ",");
-				book[j].id = atoi(bid);
-				strcpy(book[j].name,bname);
-				strcpy(book[j].author,bauth);
-				book[j].price = atoi(bprice);
-				j++;
-			}
-			fclose(fcs);
-			record:
 			printf("\nEnter Book ID to Edit: ");
-			scanf("%d",&recnum);
-			int foundflag=0;
+			scanf("%d",&inid);
 			for(int s=0;s<j;s++){
-				if(recnum==book[s].id){
+				if(inid==book[s].id){
 					foundflag++;
 					printf("Editing Data for Book ID: %d & Book Name: %s",book[s].id,book[s].name);
 					printf("\n\nEnter new Book Id: ");
 					scanf("%d",&book[s].id);
 					getchar();
-					printf("\nEnter New Book name: ");
+					printf("Enter New Book name: ");
 					gets(book[s].name);
-					printf("\nEnter new Book Author: ");
+					printf("Enter new Book Author: ");
 					gets(book[s].author);
-					printf("\nEnter new Book Price (in rupee): ");
+					printf("Enter new Book Price (in rupee): ");
 					scanf("%d",&book[s].price);
 					break;
 				}
@@ -81,33 +84,23 @@ int choiceCase(int choice){
 					fprintf(fcs, "%d,%s,%s,%d\n", book[s].id,book[s].name,book[s].author,book[s].price);
 				}
 				fclose(fcs);
-				printf("Returning to Main Menu..");
+				printf("\nReturning to Main Menu..");
 				sleep(3);
 				return 0;
 			} 
 			else {
-				printf("Book ID not found!");
-				printf("Returning to Main Menu..");
-				sleep(3);
+				notfound();
 				return 0;
 			}
 
 		case 3:
-			system("cls");
-			fcs = fopen("data.csv","r");
-			printf("\nReading Books Record..\n");
-			char line2[1024];
-			char *bid2,*bname2,*bauth2,*bprice2;
-			while (fgets(line2, 1024, fcs)!=NULL){
-				bid2=strtok(line2, ",");
-				bname2=strtok(NULL, ",");
-				bauth2=strtok(NULL, ",");
-				bprice2=strtok(NULL, ",");
+			printf("\n");
+			for(int s=0;s<j;s++){
 				printf("**************************");
-				printf("\n\nBook ID: %s \nBook Name: %s \nBook Author: %s \nBook Price: %s\n",bid2,bname2,bauth2,bprice2);
-				printf("**************************");
+				printf("\n\nBook ID: %d \nBook Name: %s \nBook Author: %s \nBook Price: %d\n",book[s].id,book[s].name,book[s].author,book[s].price);
+				printf("\n**************************");
+			
 			}
-			fclose(fcs);
 			int exit1;
 			wait:
 			printf("\nPress 0 to return to main menu: ");
@@ -116,39 +109,21 @@ int choiceCase(int choice){
 				return 0;
 			else
 				goto wait;
+
 		case 4:
-			system("cls");
-			fcs = fopen("data.csv","r");
-			char line3[1024];
-			int jj=0,delid;
-			char *bid3,*bname3,*bauth3,*bprice3;
-			printf("\nReading Books Record..");
-			while (fgets(line3, 1024, fcs)!=NULL){
-				bid3 = strtok(line3,",");
-				bname3=strtok(NULL, ",");
-				bauth3=strtok(NULL, ",");
-				bprice3=strtok(NULL, ",");
-				book[jj].id = atoi(bid3);
-				strcpy(book[jj].name,bname3);
-				strcpy(book[jj].author,bauth3);
-				book[jj].price = atoi(bprice3);
-				jj++;
-			}
-			fclose(fcs);
 			printf("\nEnter the Book ID to delete: ");
-			scanf("%d",&delid);
-			int foundflag2=0;
+			scanf("%d",&inid);
 			int ss;
-			for(ss=0;ss<=jj;ss++){
-				if(delid==book[ss].id){
-					foundflag2++;
+			for(ss=0;ss<=j;ss++){
+				if(inid==book[ss].id){
+					foundflag++;
 					break;
 				}
 			}
-			if (foundflag2==1){
+			if (foundflag==1){
 				printf("\nWriting Changes.. ");
 				fcs = fopen("data.csv","w");
-				for(int kk=0;kk<jj;kk++){
+				for(int kk=0;kk<j;kk++){
 					if(kk==ss){
 						kk++;
 						fprintf(fcs, "%d,%s,%s,%d\n", book[kk].id,book[kk].name,book[kk].author,book[kk].price);
@@ -162,28 +137,47 @@ int choiceCase(int choice){
 				return 0;
 			} 
 			else {
-				printf("Book ID not found!");
-				printf("\nReturning to Main Menu..");
-				sleep(3);
+				notfound();
 				return 0;
 			}
-
-			 
+		case 5:
+			printf("Enter The Book ID to fetch Details: ");
+			scanf("%d",&inid);
+			for(ss=0;ss<=j;ss++){
+				if(inid==book[ss].id){
+					foundflag++;
+					break;
+				}
+			}
+			if (foundflag==1){
+				printf("**************************");
+				printf("\n\nBook ID: %d \nBook Name: %s \nBook Author: %s \nBook Price: %d\n",book[ss].id,book[ss].name,book[ss].author,book[ss].price);
+				printf("\n**************************");
+				printf("\nPress 0 to return to main menu: ");
+				int exit2;
+				scanf("%d",&exit2);
+				if (exit1==0)
+					return 0;
+				else
+					goto wait;
+			}
+			else
+				notfound();
 	}
 }
 
 int main(){
 	START:system("cls");
 	int opt;
-	printf("\n\n\t\tWelcome To Bookshop Management System");
-	printf("\n\nWhat Would like to Do?");
+	printf("\n\n\n\t\tWelcome To Bookshop Management System");
+	printf("\n\n\nWhat Would like to Do?\n");
 	printf("\n1. Add Book Record");
 	printf("\n2. Edit A Book Record");
 	printf("\n3. Show the Entire Book Record");
 	printf("\n4. Delete A Book Record");
 	printf("\n5. Search a Book Record");
 	printf("\n0. For Exit");
-	printf("\nPlease enter your Option: ");
+	printf("\n\nPlease enter your Option: ");
 	scanf("%d",&opt);
 	checkopt: 
 	if (opt==0){
@@ -195,7 +189,7 @@ int main(){
 		goto START;
 	}
 	else{
-		printf("Please Enter A Valid number or press 0 to exit. ");
+		printf("\nPlease Enter A Valid number or press 0 to exit. ");
 		scanf("%d",opt);
 		goto checkopt;
 	}
