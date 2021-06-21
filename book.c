08350050ae9d79397155d276503239870a2df4aa
Kunal Agrawal <kevin.john77-01@yahoo.com>
2021-06-21 16:58:38 +0530

    Adding Records Completed

--- book.c
+++ book.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>	
+#include <time.h>
 
 struct bookinfo{
 	char name[30],author[20];
 	float price;
-	int num;
-};
+	int id;
+}book[1000];
+	
 int choiceCase(int choice){
 	FILE *fcs;
 	switch (choice){
-		case 0: 
-			printf("Exiting..");
-			break;
 		case 1: 
-			fcs = fopen("data.csv",'a+');
+			system("cls");
+			int bnum,i;
+			fcs = fopen("data.csv","a+");
+			printf("\nNumber Of Books to Add: ");
+			scanf("%d",&bnum);
+			for(i=0;i<bnum;i++){
+				printf("Enter Book Id: ");
+				scanf("%d",&book[i].id);
+				getchar();
+				printf("\nEnter Book name: ");
+				gets(book[i].name);
+				printf("\nEnter Book Author: ");
+				gets(book[i].author);
+				printf("\nEnter Book Price (in rupee): ");
+				scanf("%d",&book[i].price);
+				fprintf(fcs, "\n%d,%s,%s,%d", book[i].id,book[i].name,book[i].author,book[i].price);
+			}
+			fclose(fcs);
+			printf("Started Writing.");
+			
+			printf("\nCompleted Writing");
+			printf("\nGoing back to main menu..");
+			sleep(3);
+			break;
 
 
 
@@ -22,9 +44,9 @@ int choiceCase(int choice){
 }
 
 int main(){
-	system("cls");
+	START:system("cls");
 	int opt;
-	START: printf("\n\n\t\tWelcome To Bookshop Management System");
+	printf("\n\n\t\tWelcome To Bookshop Management System");
 	printf("\n\nWhat Would like to Do?");
 	printf("\n1. Add Book Record");
 	printf("\n2. Edit A Book Record");
@@ -34,11 +56,20 @@ int main(){
 	printf("\n0. For Exit");
 	printf("\nPlease enter your Option: ");
 	scanf("%d",&opt);
-	if((opt<6)&&(opt>=0))
+	checkopt: 
+	if (opt==0){
+		printf("Exiting..");
+		goto END;
+	}
+	else if((opt<6)&&(opt>=1)){
 		choiceCase(opt);
-	else{
-		printf("Please Enter A Valid number");
 		goto START;
 	}
+	else{
+		printf("Please Enter A Valid number or press 0 to exit. ");
+		scanf("%d",opt);
+		goto checkopt;
+	}
+	END:
 	return 0;
 }
